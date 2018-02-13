94525e39625087b8dd952984d1296c937737ff80
Alberto Mori <albi.mo87@hotmail.it>
2018-02-13 15:50:48 +0100

    client luis

---
+++ LUISdemo01.ConsoleApp/LuisClient.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace LUISdemo01.ConsoleApp
+{
+    public class LuisClient : IDisposable
+    {
+        private readonly string _appId = "762ba9dc-63e2-477a-8b6a-363daadb2c73";
+
+        private readonly string _subscriptionKey = "f0c0d8fd970b4dec88031b46da569c21";
+
+        private readonly string _baseEndpoint = "https://westeurope.api.cognitive.microsoft.com/luis/v2.0/apps/";
+
+        private bool _isStaging;
+
+        private HttpClient _client;
+
+        public LuisClient()
+            : this(false)
+        {
+            
+        }
+
+        public LuisClient(bool isStaging)
+        {
+            _client = new HttpClient();
+            _isStaging = isStaging;
+        }
+
+        public virtual async Task<string> SearchAsync(string query)
+        {
+            query = HttpUtility.UrlEncode(query);
+            string endpoint = $"{_baseEndpoint}{_appId}?subscription-key={_subscriptionKey}&verbose=true&timezoneOffset=0&q={query}";
+            if (_isStaging)
+            {
+                endpoint = $"{endpoint}&staging=true";
+            }
+
+            var response = await _client.GetAsync(endpoint);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public void Dispose()
+        {
+            if (_client != null)
+            {
+                _client.Dispose();
+            }
+
+            GC.SuppressFinalize(this);
+        }
+    }
+}
--- LUISdemo01.ConsoleApp/Program.cs
+++ LUISdemo01.ConsoleApp/Program.cs
@@ -6,7 +6,23 @@ namespace LUISdemo01.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting searching...");
+
+            Request();
+
+            Console.ReadLine();
+        }
+
+        static async void Request()
+        {
+            using (var luis = new LuisClient())
+            {
+                Console.Write("Fai la tua domanda: ");
+                string query = Console.ReadLine();
+                string response = await luis.SearchAsync(query);
+
+                Console.WriteLine(response);
+            }
         }
     }
 }
